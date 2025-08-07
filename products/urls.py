505f979b280ddeec660040a0390636db48bdbc84
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-07 20:11:23 +0530

    23856

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
 ]
+#Link custom 404 handler
+handler404='your-app-name.views'
