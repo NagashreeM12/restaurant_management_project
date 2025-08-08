9374d5bbc94e97557c08a30487ceb46c42e21352
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-08 11:24:24 +0530

    23858

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import *
 from django.contrib import admin
 from myapp import views
+from myapp import homepage
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.homepage,name='homepage'),
     path('admin/',admin.site.urls),
 
 ]
