64c72fbe0c410beda67de8580433054f83a4fb52
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-15 17:51:39 +0530

    32106

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    #Render the homepage template that shows the prominent logo
+    return render(request,"home/homepage.html")
