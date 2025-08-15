6f1a1c41d46d0169cc551bcdc264e0986d84243c
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-15 18:26:32 +0530

    32104

--- account/views.py
+++ account/views.py
@@ -3,3 +3,4 @@ from django.conf import settings
 def homepage(request):
     phone_number=getattr(settings,'RESTAURANGT_PHONE','N/A')
     return render(request,'home/index.html',{'phone_number':phone_number})
+    path
