7f7a6f48f593826ed6ec4641c74cfcf683c09163
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-17 18:11:14 +0530

    33591

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from myapp import views
 #from myapp import homepage
 
 urlpatterns = [
-    path('menu/',views.menu_items_view, name='menu'),
-    path('',home,name='home'),
-    ]
+    path("",views.home,name="home"),
+]
