7e2e319bf6e2dcbd50198c065819233535230074
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-17 17:54:13 +0530

    33592

--- home/views.py
+++ home/views.py
@@ -1,14 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.db import DatabaseError
-def reservations(request):
-    try:
-        #If you later fetch data from DB,put it here
-        #Example:reservations_list=Reservation.objects.all()
-        return render(request,'home/reservations.html')
-    except DatabaseError as db_err:
-        #Handles any DB-related issues
-        return HttpResponse(f"Database error occured:{db_err}",status=500)
-        #Handles unexpected errors
-        return HttpResponse(f"An unexpected error occured:{e}",status=500)
+from datetime import datetime
+def get_current_year(request):
+    return {'current_year':datetime.now().year}
 
