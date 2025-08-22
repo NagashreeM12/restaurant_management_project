bf289157dae6cdc49f7e91a1629f180ba4892394
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-22 19:27:55 +0530

    36592

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Menu,Order
 # Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('name','description','price')
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('customer_name','dish','quantity','created_at')
