3886d6968c75eefb025784f01557d4e18d29b0e1
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-22 19:52:34 +0530

    36591

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Order,OrderItem
 # Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    extra=1
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('id','customer','total_amount','status','created_at')
+    list_filter=('status','created_at')
+    inlines=[OrderItemInline]
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=('order','menu_item',''quantify')
