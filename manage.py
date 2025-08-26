e5f5516c99e7e7685fc00222d5a1b6f3b504a77c
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-26 12:02:29 +0530

    39187

--- manage.py
+++ manage.py
@@ -2,21 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+from django.db import models
+class RestaurantLocation(models.Model):
+    #Street address of the restaurant
+    address=models.CharField(max_length=255)
+    #City name
+    city=models.CharField(max_length=100)
+    #state name
+    state=models.CharField(max_length=100)
+    #Zip/postal code
+    zipcode=models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.address},{self.city},{self.state}-{self.zipcode}"
