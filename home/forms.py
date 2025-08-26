56b5a2d16bc162991df3aeb1681f29df3aaea3f1
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-26 12:43:25 +0530

    39186

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,12 @@
 #home/forms.py
-from django import forms
-from.models import Contact
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model=Contact
-        fields=['name','email'] #only ask for email and name
+from django.shortcuts import render,redirect
+from.forms import ContactForm
+def contact_view(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save() #save to database
+            return redirect('contact') #redirect after success
+        else:
+            form=ContactForm()
+        return render(request,'home/contact.html',{'form':form})
