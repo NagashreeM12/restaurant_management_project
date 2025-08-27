16186809c6abefac471c40474e0eb40d6d26feb4
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-27 17:38:49 +0530

    39183

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class Contact(models.Model):
-    name=models.CharField(max_length=100)
-    email=models.EmailField()
-    created_at=models.DateTimeField(auto_now_add=True)
+class RestaurantInfo(models.Model):
+    name=models.CharField(max_length=150,default="My Restaurant")
+    address=models.TextField()
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    zipcode=models.CharField(max_length=100)
     def __str__(self):
-        return f"{self.name}-{self.email}"
+        return self.name
