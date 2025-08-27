c5ca4174fcbb99b54ae4dc6486a8deab9591e3bd
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-27 16:21:27 +0530

    39185

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('home.urls')),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('feedback/success/',views.feedback_success,name='feedback_success'),
     path('menu/',MenuView.as_view(),name='menu')
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
