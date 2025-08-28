a7ac02b0ea25c5a55ef123702c87a2ad8048fdbf
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-28 17:41:24 +0530

    39778

--- home/views.py
+++ home/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from .forms import ContactForm
-def home_view(request):
-    if request.method=="POST":
-        form=ContactForm(request.POST)
-        if form.is_valid():
-            form.save()#save to database
-            return redirect("home") #redirect to homepage after submission
-        else:
-            form=ContactForm()
-        return render(request,"home/index.html",{"form":form})
+from .models import RestaurantAddress
+def homepage(request):
+    address=RestaurantAddress.objects.first() # Fetch first address
+    return render(request,"home/homepage.html",{"address":address})
     
 
 
