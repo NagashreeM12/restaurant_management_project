ea668490aef55906514b3779b4415b55ad3d67c2
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-28 17:38:49 +0530

    39778

--- home/models.py
+++ home/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class RestaurantInfo(models.Model):
+class RestaurantAddress(models.Model):
     name=models.CharField(max_length=150,default="My Restaurant")
     address=models.TextField()
     city=models.CharField(max_length=100)
     state=models.CharField(max_length=100)
     zipcode=models.CharField(max_length=100)
     def __str__(self):
-        return self.name
+        return  f"{self.street},{self.city},{self.state}-{self.zipcode}"
