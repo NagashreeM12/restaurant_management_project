0114128d1da7165eaf849faba24dbddebccec686
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-29 18:33:58 +0530

    39775

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -121,6 +121,14 @@ USE_I18N = True
 USE_TZ = True
 MEDIA_URL='/media'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+#Email Settings
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='webites@gmail.com'
+EMAIL_HOST_PASSWORD='900@abcD'
+DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
 
 
 # Static files (CSS, JavaScript, Images)
