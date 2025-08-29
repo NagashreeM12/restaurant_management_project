677b85f48a1771175ee0e00abfe5b6c8995ad653
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-29 18:51:10 +0530

    39775

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .import views
-#from myapp import homepage
-
-urlpatterns = [
-    path("",views.homepage,name="homepage"),
+from.views import contact_view
+urlpatterns=[
+    path('contact/',contact_view,name='contact'),
+    path('contact/success/',lambda request:render(request,"home/contact_success.html"),name='contact_success'),
+    
+]
 ]
