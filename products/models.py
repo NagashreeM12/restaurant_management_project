2e4363a2c9382eeba4813662cfaa22c79aa82769
NagashreeM12 <155900255+NagashreeM12@users.noreply.github.com>
2025-08-29 18:14:55 +0530

    39776

--- products/models.py
+++ products/models.py
@@ -4,12 +4,6 @@ class MenuItem(models.Model):
     description=models.TextField(blank=True,null=True)
     price=models.DecimalField(max_digits=6,decimal_places=2)
     image=models.ImageField(upload_to='menu_images/',blank=True,null=True)
-    #Image upload field
-    image=models.ImageField(
-        upload_to='menu_images/',#stores in MEDIA_ROOT
-        blank=True
-        null=True
-    )
     def __str__(self):
         return self.name
 
