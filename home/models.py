9b44d47d2d7e3ad636b41153fb36099eab391183
Nagashreem M <nagashreem356@gmail.com>
2025-08-30 20:03:25 +0530

    Added restaurant phone number to homepage

--- home/models.py
+++ home/models.py
@@ -1,10 +1,10 @@
+# models.py
 from django.db import models
 
-class RestaurantAddress(models.Model):
-    name=models.CharField(max_length=150,default="My Restaurant")
-    address=models.TextField()
-    city=models.CharField(max_length=100)
-    state=models.CharField(max_length=100)
-    zipcode=models.CharField(max_length=100)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)  # Add phone number field
+
     def __str__(self):
-        return  f"{self.street},{self.city},{self.state}-{self.zipcode}"
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,29 +1,14 @@
 # views.py
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Restaurant
 
-# View to handle adding an item to the cart
-def add_to_cart(request, item_id):
-    # Retrieve the current cart from the session (empty list if not present)
-    cart = request.session.get('cart', [])
+def homepage(request):
+    # Fetch the restaurant object (assuming there's only one restaurant)
+    restaurant = Restaurant.objects.first()
     
-    # Add the new item to the cart
-    cart.append(item_id)
-    
-    # Save the updated cart back to the session
-    request.session['cart'] = cart
-    
-    # Redirect to the cart count view after adding an item
-    return redirect('cart_count')  # Adjust this as per your URL name
+    # Pass the restaurant's phone number to the template
+    return render(request, 'index.html', {'phone_number': restaurant.phone_number})
 
-# View to display the cart count (total number of items)
-def cart_count(request):
-    # Get the total number of items in the cart
-    cart = request.session.get('cart', [])
-    cart_count = len(cart)  # Count the number of items in the cart
-    
-    # Render the homepage template with the cart count
-    return render(request, 'index.html', {'cart_count': cart_count})
 
 
 
