28c4a473d58c0577d8afbf784abf3512d8695251
Nagashreem M <nagashreem356@gmail.com>
2025-08-30 19:13:29 +0530

    Added cart functionality and displayed total items on the homepage

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import contact_view
+from .views import add_to_cart, cart_count
 
 urlpatterns = [
-    path('contact/', contact_view, name='contact'),
+    path('add_to_cart/<int:item_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart_count, name='cart_count'),
 ]
+
--- home/views.py
+++ home/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ContactForm
 
-def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            # Process the data (save/send email etc.)
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            return HttpResponse("Thank you! Your message has been received.")
-    else:
-        form = ContactForm()
+def add_to_cart(request, item_id):
+    cart = request.session.get('cart', [])
+    cart.append(item_id)
+    request.session['cart'] = cart
+    return HttpResponse("Item added to cart")
 
-    return render(request, 'home/contact.html', {'form': form})
+def cart_count(request):
+    cart = request.session.get('cart', [])
+    return render(request, 'cart.html', {'cart_count': len(cart)})
+
 
 
 
