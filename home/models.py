a43981137c2c35f56e1b18444ce831aecad54631
Nagashreem M <nagashreem356@gmail.com>
2025-09-15 19:48:37 +0530

    Task: Added MenuCategory model in home app

--- home/models.py
+++ home/models.py
@@ -2,12 +2,10 @@
 # models.py
 from django.db import models
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.TextField()
-    phone_number = models.CharField(max_length=15)
-    logo = models.ImageField(upload_to='restaurant_logos/', null=True, blank=True)  # Logo image field
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
 
